96d103e48cd578566c02965b6bb9e0d893f2e9a8
life-cfg <72955390+life-cfg@users.noreply.github.com>
2021-01-30 15:44:23 -0600

    Update weaponsinallslots.js

--- src/weaponsinallslots.js
+++ src/weaponsinallslots.js
@@ -23,7 +23,7 @@ exports.mod = () => {
             fileData._props.Slots[2]._props.filters[0].Filter = ["5447b5fc4bdc2d87278b4567","5447b5f14bdc2d61278b4567","5447bedf4bdc2d87278b4568","5447bed64bdc2d97278b4568","5447b6194bdc2d67278b4567","5447b6094bdc2dc3278b4567","5447b5e04bdc2d62278b4567","5447b6254bdc2dc3278b4568","5447bee84bdc2dc3278b4569","5447b5cf4bdc2d65278b4567","543be6564bdc2df4348b4568"];
             };
         if (fileData._name == "Default Inventory") {
-            fileData._props.Slots[3]._props.filters[0].Filter = ["5447b5fc4bdc2d87278b4567","5447b5f14bdc2d61278b4567","5447bedf4bdc2d87278b4568","5447bed64bdc2d97278b4568","5447b6194bdc2d67278b4567","5447b6094bdc2dc3278b4567","5447b5e04bdc2d62278b4567","5447b6254bdc2dc3278b4568","5447bee84bdc2dc3278b4569","5447b5cf4bdc2d65278b4567","543be6564bdc2df4348b4568"];
+            fileData._props.Slots[3]._props.filters[0].Filter = ["5447e1d04bdc2dff2f8b4567","5447b5fc4bdc2d87278b4567","5447b5f14bdc2d61278b4567","5447bedf4bdc2d87278b4568","5447bed64bdc2d97278b4568","5447b6194bdc2d67278b4567","5447b6094bdc2dc3278b4567","5447b5e04bdc2d62278b4567","5447b6254bdc2dc3278b4568","5447bee84bdc2dc3278b4569","5447b5cf4bdc2d65278b4567","543be6564bdc2df4348b4568"];
             };
             base.data[fileData._id] = fileData;
         
